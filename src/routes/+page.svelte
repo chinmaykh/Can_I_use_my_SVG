3cc6340137438e0d1aec008670ae90af82cdcff5
Chinmay K <52078442+chinmaykh@users.noreply.github.com>
2023-12-15 16:48:00 +0000

    Same with(out) the leading slash

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<img src="logo.svg" alt="My cool logo">
+<img src="/logo.svg" alt="My cool logo" width="1076" height="1080">
